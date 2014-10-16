fix convergence and a mini bug in the gradient for regularizer

CreateReplacementDistribution.py
@@ -4,23 +4,26 @@ Created on Mon Jul 07 00:12:22 2014
 
 @author: Adith
 """
-import cPickle
-import scipy.sparse
 import sys
+import collections
+import cPickle
 import numpy
 import scipy
-import collections
+import scipy.sparse
+import nltk
 
-f = open('Brown_trigrams.pkl','rb')
+f = open('../data/Brown_trigrams.pkl','rb')
 trigrams = cPickle.load(f)
 f.close()
 
-stopWords = set()
-stopWFile = open('/media/adith/DATA/datasets/nltk_data/corpora/stopwords/english','r')
-for line in stopWFile:
-    token = line.strip().lower()
-    stopWords.add(token)
-stopWFile.close()
+# stopWords = set()
+# stopWFile = open('/media/adith/DATA/datasets/nltk_data/corpora/stopwords/english','r')
+# for line in stopWFile:
+#     token = line.strip().lower()
+#     stopWords.add(token)
+# stopWFile.close()
+
+stopWords = set(nltk.corpus.stopwords.words('english'))
 
 unigramFreq = collections.Counter()
 for elem, cnt in trigrams.iteritems():
@@ -123,11 +126,13 @@ for i,j in possiblePairs:
         print ".",
         sys.stdout.flush()
 
-f = open('Brown_vocab.pkl','wb')
+f = open('../data/Brown_vocab.pkl','wb')
 cPickle.dump((vocabulary, vocabList, contextWordDict, wordContextDict), f, -1)
 f.close()
 
 probabilities = probabilities.tocoo()
-f = open('Brown_prob','wb')
+f = open('../data/Brown_prob','wb')
 numpy.savez(f, probabilities.row, probabilities.col, probabilities.data)
 f.close()
+
+

NaiveEmbed.py
@@ -13,11 +13,15 @@ import numpy
 import scipy.spatial.distance
 import itertools
 
-f = open('Brown_vocab.pkl','rb')
+
+# this is unnecessary
+'''
+f = open('../data/Brown_vocab.pkl','rb')
 tup = cPickle.load(f)
 vocabulary = tup[0]
 vocabList = tup[1]
 f.close()
+'''
 
 numDim = 2
 if len(sys.argv) > 1:
@@ -31,31 +35,32 @@ dp_lambda = 1.0
 if len(sys.argv) > 3:
     dp_lambda = float(sys.argv[3])
 
-numWords = len(vocabulary)
 
-f = open('Brown_prob','rb')
+f = open('../data/Brown_prob','rb')
 npzFiles = numpy.load(f)
-true_probabilities = scipy.sparse.coo_matrix((npzFiles['arr_2'], (npzFiles['arr_0'], npzFiles['arr_1'])), shape=(numWords, numWords))
+true_probabilities = scipy.sparse.coo_matrix((npzFiles['arr_2'], (npzFiles['arr_0'], npzFiles['arr_1'])))
+numWords = true_probabilities.shape[0]
 probabilities = true_probabilities.toarray()
 f.close()
 
+ops = {'maxiter': 10000, 'disp': True, 'ftol': 1e-5, 'gtol': 1e-5, 'maxcor': 100}
+
 def DPMeans(currEmbedding):
     global numWords, dp_lambda
-
     print "\n"
     sys.stdout.flush()
 
     clusterAssignments = numpy.zeros(numWords)
-
+    # initial there is only one cluster
     clusterMeans = numpy.mean(currEmbedding, axis=0)
     clusterMeans = numpy.atleast_2d(clusterMeans)
 
-    potential = -1.0
-    prev_potential = -1.0
-    while (potential < prev_potential) or (prev_potential < 0.0):
-        prev_potential = potential;
+    potential = None
+    prev_potential = None
+    improvement = 1
+    # add an error tolerance
+    while improvement > ops['ftol']:
         potential = 0.0;
-
         for i in xrange(numWords):
             distances = scipy.spatial.distance.cdist(numpy.atleast_2d(currEmbedding[i,:]), numpy.atleast_2d(clusterMeans), 'sqeuclidean')
             distances = distances.ravel()
@@ -75,9 +80,15 @@ def DPMeans(currEmbedding):
             if cluster_size > 0:
                 clusterMeans[i,:] = numpy.mean(currEmbedding[clusterAssignments == i,:], axis=0)
             else:
+                # how can this happen
+                print 'error in dp means, no clusters can be empty'
                 clusterMeans[i,:] = 0
-
-        print potential, numpy.shape(clusterMeans)[0]
+        if potential == 0:
+          break
+        if prev_potential is not None:
+          improvement = (prev_potential-potential)/prev_potential
+        prev_potential = potential;
+        print 'clustering', potential, numpy.shape(clusterMeans)[0]
         sys.stdout.flush()
 
     return (clusterMeans, clusterAssignments)
@@ -89,12 +100,14 @@ def KLDiv(currEmbeddingFlat, clusterMeans, clusterAssignments):
     distanceMatrix = 1 + scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(currEmbedding, 'sqeuclidean'))
     logDistanceMatrix = numpy.log(distanceMatrix)
     weightedDistanceMatrix = numpy.multiply(probabilities, logDistanceMatrix)
+    # minimize KL divergence, this should be minus
     Objective = weightedDistanceMatrix.sum()
 
     invDistanceMatrix = numpy.reciprocal(distanceMatrix)
     Z = invDistanceMatrix.sum(axis=1)
     logZ = numpy.log(Z)
 
+    # this should also be minus
     Objective += logZ.sum()
 
     currentDistribution = invDistanceMatrix / Z[:,numpy.newaxis]
@@ -115,30 +128,42 @@ def KLDiv(currEmbeddingFlat, clusterMeans, clusterAssignments):
             clusterEmbed = clusterMeans[clusterAssignments[k],:]
             Objective += reg_lambda*scipy.spatial.distance.cdist(numpy.atleast_2d(wordEmbed), numpy.atleast_2d(clusterEmbed), 'sqeuclidean')
             Objective += reg_lambda*dp_lambda*numpy.shape(clusterMeans)[0]
-            gradient[k,:] += 2.0*(wordEmbed-clusterEmbed)
+            # a bug here, missed reg_lambda
+            gradient[k,:] += 2.0*reg_lambda*(wordEmbed-clusterEmbed)
 
     gradientFlat = numpy.reshape(gradient, numWords*numDim)
     return (Objective, gradientFlat)
 
-ops = {'maxiter': 10000, 'disp': True, 'ftol': 1e-10, 'gtol': 1e-10, 'maxcor': 100}
 
-X = numpy.random.randn(numWords, numDim)
+# add numDim so that dp_lambda roughly makes sense
+X = numpy.random.randn(numWords, numDim)/numpy.sqrt(numDim)
+# for i in xrange(10):        #TODO: Truly iterate to convergence
 
-for i in xrange(10):        #TODO: Truly iterate to convergence
+prevObjective, currObjective = None, None
+improvement, iteration = 1, 0
+while improvement > ops['ftol']:
     clusterMeans, clusterAssignments = DPMeans(X)
-    f = open('Brown_embed.'+str(i),'wb')
+    f = open('../data/Brown_embed.%d.%d' % (iteration, numDim),'wb')
     numpy.savez(f, X, clusterMeans, clusterAssignments)
     f.close()
 
     start_x = numpy.reshape(X, numWords*numDim)
-    Result = scipy.optimize.minimize(fun = KLDiv, x0 = start_x, args=(clusterMeans, clusterAssignments), method = 'L-BFGS-B', jac = True, tol = 1e-10, options = ops)
-    print "ITERATION", i, Result['success'], Result['status'], Result['message'], Result['nit']
+    Result = scipy.optimize.minimize(fun=KLDiv, x0=start_x,
+        args=(clusterMeans, clusterAssignments), method='L-BFGS-B',
+        jac=True, tol=ops['ftol'], options=ops)
+    
+    X = numpy.reshape(Result['x'], (numWords, numDim))
+    # only 1 value
+    currObjective = Result['fun'].sum()
+    if prevObjective is not None:
+      improvement = (prevObjective - currObjective) / prevObjective
+    prevObjective = currObjective
+    print "ITERATION", iteration, Result['success'], Result['status'], Result['message'], Result['nit'], improvement
+    iteration += 1
     sys.stdout.flush()
 
-    X = numpy.reshape(Result['x'], (numWords, numDim))
-
 clusterMeans, clusterAssignments = DPMeans(X)
-f = open('Brown_result','wb')
+f = open('../data/Brown_result.%d' % numDim,'wb')
 numpy.savez(f, X, clusterMeans, clusterAssignments)
 f.close()
 